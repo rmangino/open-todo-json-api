de8ff294228e1702cfcddef2da517fe54cb83c70
Reed Mangino <reed@themanginos.com>
2015-09-01 12:41:18 -0400

    Modify migrations so that Users are created before Lists.

--- db/schema.rb
+++ db/schema.rb
@@ -28,9 +28,9 @@ ActiveRecord::Schema.define(version: 20150829143146) do
   create_table "items", force: :cascade do |t|
     t.string   "title"
     t.integer  "list_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean  "complete"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "complete",   default: false, null: false
   end
 
   add_index "items", ["list_id"], name: "index_items_on_list_id"
